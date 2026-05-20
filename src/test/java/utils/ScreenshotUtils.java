b36fdc217e1142f81b29af4185d5a89bea69b9f6
bhavna <bhavna.amul@gmail.com>
2026-05-20 13:30:41 +0530

    utility related code

--- src/test/java/utils/ScreenshotUtils.java
+++ src/test/java/utils/ScreenshotUtils.java
@@ -1,5 +1,28 @@
 package utils;
 
+import java.io.File;
+import driver.DriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import driver.DriverManager;
+
 public class ScreenshotUtils {
 
+	public static void captureScreenshot(String fileName) {
+
+		try {
+
+			File source = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+
+			File destination = new File("screenshots/" + fileName + ".png");
+
+			FileUtils.copyFile(source, destination);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/utils/WaitUtils.java
+++ src/test/java/utils/WaitUtils.java
@@ -1,5 +1,19 @@
 package utils;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import driver.DriverManager;
+
 public class WaitUtils {
+	public static void waitForElement(By locator) {
+
+		WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(20));
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	}
 
 }
